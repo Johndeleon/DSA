2cf58f3a8f3a1809dbf7a1fcfdc54f62cc94b00e
Johndeleon <huynthilandeleon@gmail.com>
2016-09-08 19:03:12 +0800

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.Random;
+public class LinearSearch {
+
+	
+	int size;
+	int [] array;
+	Random r = new Random();
+	int find;
+	int ans=0;
+	int index;
+	
+	public static void main (String[] args){
+		
+		LinearSearch link = new LinearSearch();
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("enter number of integers ");
+		link.size= input.nextInt();
+		
+		link.array= new int [link.size];
+
+		
+		link.getRand();
+		
+		System.out.print("enter number to find ");
+		link.find=input.nextInt();
+		
+		link.search();
+		
+		if (link.ans==1){
+		System.out.println("number "+link.find+" at index: "+link.index);
+		
+		}
+		
+		else{
+		System.out.println("number not found");	
+		}
+		
+		
+		
+	}
+//--------------------------------------------------	
+	public void getRand(){
+		
+		for (int i=0;i<size;i++){
+			array[i]=r.nextInt(size);
+		}
+	}
+//--------------------------------------------------	
+	
+	public void search(){
+		
+		for (int i=0;i<size;i++){
+			if (find==array[i]){
+				ans=1;
+				index=i;
+			}
+			
+		}
+	}
+	
+	
+	
+	
+}
