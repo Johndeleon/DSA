7d5c5cc74c647c5069e0e5802e3337e593d6de25
Johndeleon <huynthilandeleon@gmail.com>
2016-09-21 15:56:29 +0800

    Add files via upload

---
+++ SumAveMax.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class SumAveMax {
+    
+     int size=0;
+     int[] array;
+     int sum=0;
+     double ave=0;
+     int max=0;
+ 
+     Random r = new Random();
+     
+    public static void main (String[] args){
+       
+        
+        SumAveMax link = new SumAveMax();
+         
+         
+        Scanner input=new Scanner(System.in);
+        
+        System.out.print("enter size of array: ");
+       link.size=input.nextInt();
+        
+       link.array = new int [link.size];
+        
+      
+        
+        for (int i=0;i<link.size;i++){
+            link.array[i]=link.r.nextInt(); 
+        }
+        
+        
+        link.getTotal();
+        link.getAve();
+        link.getMax();
+        
+        
+                
+          for (int i=0;i<link.size;i++){
+            System.out.println("index "+i+": "+link.array[i]);
+        }
+          
+          System.out.println("\ntotal: "+link.sum);
+          System.out.println("average: "+link.ave);
+          System.out.println("max: "+link.max);
+          
+    }
+    
+    public void getTotal(){
+        for (int i=0;i<size;i++){
+        sum=sum+array[i];
+    }
+    }
+    
+    public void getAve(){
+     ave=sum/array.length;
+}
+    
+    public void getMax(){
+     for (int i=0;i<size;i++){
+        if (array[i]>max){
+            max=array[i];
+        }
+    }
+}
+}
+
